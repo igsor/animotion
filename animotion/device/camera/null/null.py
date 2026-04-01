12ec1cdf5005705ce90cd99364d4e5631dc0fa71
Matthias Baumgartner <dev@igsor.net>
2026-04-01 17:58:19 +0200

    feat: camera interface and dummy implementation

---
+++ animotion/device/camera/null/null.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass, field
+from pathlib import Path
+from random import random
+
+import numpy as np
+from numpy import typing as npt
+
+from animotion.domain.camera import Camera
+
+
+@dataclass
+class Null(Camera):
+    resolution: tuple[int, int] = field(default=(320, 240))
+    preview_switch_threshold: float = field(default=0.9)
+
+    _last_image: npt.NDArray = field(init=False)
+
+    def __post_init__(self) -> None:
+        self._last_image = np.random.random(self.resolution)
+
+    def get_low_resolution_image(self) -> npt.NDArray:
+        if random() > self.preview_switch_threshold:
+            self._last_image = np.random.random(self.resolution)
+        return self._last_image
+
+    def start_video(self, path: Path) -> None:
+        pass
+
+    def stop_video(self) -> None:
+        pass
---
+++ animotion/domain/camera.py
@@ -0,0 +1,18 @@
+from abc import ABC, abstractmethod
+from pathlib import Path
+
+from numpy import typing as npt
+
+
+class Camera(ABC):
+    @abstractmethod
+    def get_low_resolution_image(self) -> npt.NDArray:
+        pass
+
+    @abstractmethod
+    def start_video(self, path: Path) -> None:
+        pass
+
+    @abstractmethod
+    def stop_video(self) -> None:
+        pass

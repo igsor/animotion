d83ee5e43739b7992abc984b661cf86b78b138bd
Matthias Baumgartner <dev@igsor.net>
2026-04-01 18:10:36 +0200

    feat: picamera implementation

---
+++ animotion/device/camera/picamera/config.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True, slots=True)
+class VideoConfig:
+    resolution: tuple[int, int]
+    format: str  # RGB888, BGR888, XBGR8888, XRGB8888, YUV420
+    encoder: str  # H264, JPEG, MJPEG, NULL
+    bitrate: int
+    quality: str  # VERY_LOW, LOW, MEDIUM, HIGH, VERY_HIGH
+
+
+@dataclass(frozen=True, slots=True)
+class PreviewConfig:
+    resolution: tuple[int, int]
+    format: str
+
+
+@dataclass(frozen=True, slots=True)
+class Config:
+    video: VideoConfig
+    preview: PreviewConfig

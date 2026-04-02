827a095b6a8f4456af4c93cfd11e0346a5203c94
Matthias Baumgartner <dev@igsor.net>
2026-04-02 11:46:59 +0200

    feat: app arguments

--- animotion/app/recorder.py
+++ animotion/app/recorder.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
+from pathlib import Path
 
 import typer
+from dependency_injector.providers import Configuration
 from dependency_injector.wiring import Provide, inject
 
 from animotion.config.animotion import AnimotionContainer
@@ -11,15 +13,40 @@ def main() -> None:
     typer.run(_main)
 
 
-def _main() -> None:
-    _init()
+def _main(
+    output_folder: Path | None = None,
+    config_file: Path | None = None,
+) -> None:
+    _init(
+        output_folder=output_folder,
+        config_file=config_file,
+    )
     _run()
 
 
-def _init() -> None:
+def _init(
+    output_folder: Path | None = None,
+    config_file: Path | None = None,
+) -> None:
     app = AnimotionContainer()
+    if config_file is not None:
+        _load_config_from_path(app.config, config_file)
+    if output_folder is not None:
+        app.config.app.video_target_folder.from_value(output_folder)
     app.logging.init_resources()
     app.wire(modules=[__name__])
+
+
+def _load_config_from_path(config: Configuration, path: Path) -> None:
+    match path.suffix:
+        case ".yml":
+            config.from_yaml(path)
+        case ".json":
+            config.from_json(path)
+        case ".ini":
+            config.from_ini(path)
+        case _:
+            raise ValueError(path.suffix)
 
 
 @inject

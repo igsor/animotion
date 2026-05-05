9dac312ee2aff746517058bc6d19806c4697c777
Matthias Baumgartner <dev@igsor.net>
2026-05-05 07:58:35 +0200

    refactor: sequential folders as app

--- animotion/app/output_tree.py
+++ animotion/app/output_tree.py
@@ -1,10 +1,21 @@
+#!/usr/bin/env python3
 from pathlib import Path
 from typing import Final
+
+import typer
 
 _INSTANCE_FOLDER_NAME_PATTERN: Final[str] = "{:05d}"
 
 
-def next_folder_in_sequence_in(root_folder: Path) -> Path:
+def main() -> None:
+    typer.run(_main)
+
+
+def _main(root_folder: Path) -> None:
+    print(_next_folder_in_sequence_in(root_folder))
+
+
+def _next_folder_in_sequence_in(root_folder: Path) -> Path:
     highest_sequence_number = (
         max(
             (
@@ -27,3 +38,7 @@ def _sequence_number_from(path: Path) -> int | None:
         return int(path.name)
     except ValueError:
         return None
+
+
+if __name__ == "__main__":
+    main()
